f8078ae694c9afda400ceb3fe7f9452bbbdce755
zizhou <18679326282@163.com>
2020-08-12 08:08:31 +0800

    在idea里面提交项目

---
+++ src/test/java/com/zizhou/gitTest.java
@@ -0,0 +1,12 @@
+package com.zizhou;
+
+/**
+ * @Description:
+ * @Author: NickXia
+ * @date: 2020/8/12 8:05
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
