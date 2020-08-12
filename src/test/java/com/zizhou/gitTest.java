89cf2d40a1f46625e8014dc3d68157bad6d4d3d9
zizhou <18679326282@163.com>
2020-08-12 08:26:44 +0800

    修改gitTest

--- src/test/java/com/zizhou/gitTest.java
+++ src/test/java/com/zizhou/gitTest.java
@@ -7,6 +7,6 @@ package com.zizhou;
  */
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World!今天天气真好啊!");
     }
 }
